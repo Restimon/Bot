b25b579fdb614eceab0f782b1a80e149c4edcbbb
Restimon <nico060901@gmail.com>
2025-05-20 01:05:49 +0200

    Update data.py

--- data.py
+++ data.py
@@ -5,17 +5,22 @@ from utils import inventaire, hp, leaderboard
 DATA_FILE = "data.json"
 
 def sauvegarder():
+    data = {
+        "inventaire": inventaire,
+        "hp": hp,
+        "leaderboard": leaderboard
+    }
     with open(DATA_FILE, "w", encoding="utf-8") as f:
-        json.dump({
-            "inventaire": inventaire,
-            "hp": hp,
-            "leaderboard": leaderboard
-        }, f, indent=4, ensure_ascii=False)
+        json.dump(data, f, indent=4, ensure_ascii=False)
 
 def charger():
     if os.path.exists(DATA_FILE):
         with open(DATA_FILE, "r", encoding="utf-8") as f:
             data = json.load(f)
+            inventaire.clear()
+            hp.clear()
+            leaderboard.clear()
+
             inventaire.update(data.get("inventaire", {}))
             hp.update(data.get("hp", {}))
             leaderboard.update(data.get("leaderboard", {}))
