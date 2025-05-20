dbdfc9481d5f18ff26cb7c1f47aee0ee2e9a4fa9
Restimon <nico060901@gmail.com>
2025-05-20 13:45:02 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -14,11 +14,9 @@ def register_profile_command(bot):
         uid = str(member.id)
 
         # Récupérer les données utilisateur dans ce serveur
-        user_data = get_user_data(guild_id, uid)
         user_inv, user_hp, user_stats = get_user_data(guild_id, uid)
-        user_hp = user_data["hp"]
-        user_stats = user_data["stats"]
         points = user_stats["degats"] + user_stats["soin"]
+
 
         # Tri du classement pour CE serveur uniquement
         server_leaderboard = leaderboard.get(guild_id, {})
