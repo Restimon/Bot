09256cf5229308a70b289e2b31ec147bcbab2e9f
Restimon <nico060901@gmail.com>
2025-05-20 15:21:48 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -15,10 +15,23 @@ def get_leaderboard(guild_id):
 def get_user_data(guild_id, user_id):
     gid = str(guild_id)
     uid = str(user_id)
-    g = str(guild_id)
-    inv = get_inventory(g).setdefault(uid, [])
-    pv = get_hp(g).setdefault(uid, 100)
-    stats = get_leaderboard(g).setdefault(uid, {"degats": 0, "soin": 0})
+
+    inv = inventaire.setdefault(gid, {}).setdefault(uid, [])
+    pv = hp.setdefault(gid, {}).setdefault(uid, 100)
+    stats = leaderboard.setdefault(gid, {}).setdefault(uid, {"degats": 0, "soin": 0})
+
+    if not isinstance(inv, list):
+        inventaire[gid][uid] = []
+        inv = inventaire[gid][uid]
+
+    if not isinstance(pv, int):
+        hp[gid][uid] = 100
+        pv = hp[gid][uid]
+
+    if not isinstance(stats, dict) or "degats" not in stats or "soin" not in stats:
+        leaderboard[gid][uid] = {"degats": 0, "soin": 0}
+        stats = leaderboard[gid][uid]
+
     return inv, pv, stats
 
 def reset_guild_data(guild_id):
