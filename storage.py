8dcb520a8e1265017b3d4072b0d9e8b7841ef8a6
Restimon <nico060901@gmail.com>
2025-05-20 02:26:13 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -15,8 +15,13 @@ def get_leaderboard(guild_id):
 def get_user_data(guild_id, user_id):
     uid = str(user_id)
     g = str(guild_id)
-    return {
-        "inv": get_inventory(g).setdefault(uid, []),
-        "hp": get_hp(g).setdefault(uid, 100),
-        "stats": get_leaderboard(g).setdefault(uid, {"degats": 0, "soin": 0}),
-    }
+    inv = get_inventory(g).setdefault(uid, [])
+    pv = get_hp(g).setdefault(uid, 100)
+    stats = get_leaderboard(g).setdefault(uid, {"degats": 0, "soin": 0})
+    return inv, pv, stats
+
+def reset_guild_data(guild_id):
+    gid = str(guild_id)
+    inventaire[gid] = {}
+    hp[gid] = {}
+    leaderboard[gid] = {}
