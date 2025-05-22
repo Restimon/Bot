734f21c79dd14dd9bdf4daaecda7dcd54d1d9815
Restimon <nico060901@gmail.com>
2025-05-22 20:54:32 +0200

    Update heal.py

--- heal.py
+++ heal.py
@@ -147,18 +147,21 @@ def register_heal_command(bot):
         sauvegarder()
         await interaction.response.send_message(embed=embed)
 
-        @heal_slash.autocomplete("item")
-        async def autocomplete_items(interaction: discord.Interaction, current: str):
-            guild_id = str(interaction.guild.id)
-            uid = str(interaction.user.id)
-            user_inv, _, _ = get_user_data(guild_id, uid)
+    @heal_slash.autocomplete("item")
+    async def autocomplete_items(interaction: discord.Interaction, current: str):
+        guild_id = str(interaction.guild.id)
+        uid = str(interaction.user.id)
+        user_inv, _, _ = get_user_data(guild_id, uid)
 
-            heal_items = sorted(set(i for i in user_inv if OBJETS.get(i, {}).get("type") == "soin" or i in ["💉", "🛡", "👟", "🪖"]))
+        SPECIAL_HEAL_ITEMS = ["💉", "🛡", "👟", "🪖", "💕"]
+        heal_items = sorted(set(
+            i for i in user_inv if OBJETS.get(i, {}).get("type") == "soin" or i in SPECIAL_HEAL_ITEMS
+        ))
 
-            if not heal_items:
-                return [app_commands.Choice(name="Aucun objet de soin", value="")]
+        if not heal_items:
+            return [app_commands.Choice(name="Aucun objet de soin", value="")]
 
-            return [
-                app_commands.Choice(name=emoji, value=emoji)
-                for emoji in heal_items if current in emoji
-            ][:25]
+        return [
+            app_commands.Choice(name=emoji, value=emoji)
+            for emoji in heal_items if current in emoji
+        ][:25]
