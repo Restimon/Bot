2b0315735ff6e69b15b44d34765f87ede157add8
Restimon <nico060901@gmail.com>
2025-05-23 04:59:23 +0200

    Create cooldowns.py

---
+++ cooldowns.py
@@ -0,0 +1,21 @@
+cooldowns = {
+    "attack": {},  # cooldowns["attack"][guild_id][user_id]
+    "heal": {}     # cooldowns["heal"][(guild_id, user_id, target_id)]
+}
+
+ATTACK_COOLDOWN = 120
+HEAL_COOLDOWN = 180
+
+import time
+
+def is_on_cooldown(guild_id, key, action):
+    now = time.time()
+    cd = cooldowns[action].get(guild_id, {})
+    if isinstance(key, tuple):
+        value = cd.get(tuple(key))
+    else:
+        value = cd.get(key)
+    if value is None:
+        return False, 0
+    remaining = int(value + (ATTACK_COOLDOWN if action == "attack" else HEAL_COOLDOWN) - now)
+    return remaining > 0, remaining
