aa04efe4d01628f426ac70a6478bb5016e426e19
Restimon <nico060901@gmail.com>
2025-05-23 05:09:19 +0200

    Update data.py

--- data.py
+++ data.py
@@ -99,10 +99,3 @@ def charger():
         print("⚠️ Le fichier data.json est corrompu ou mal formé.")
     except Exception as e:
         print(f"❌ Erreur inattendue lors du chargement : {e}")
-
-def remove_status_effects(guild_id, user_id):
-    for status_dict in [virus_status, poison_status, infection_status]:
-        if guild_id in status_dict and user_id in status_dict[guild_id]:
-            del status_dict[guild_id][user_id]
-
-
