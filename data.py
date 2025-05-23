a8b204ce89dc0b72077fdfe954df355aa8688a59
Restimon <nico060901@gmail.com>
2025-05-23 04:57:42 +0200

    Update data.py

--- data.py
+++ data.py
@@ -122,9 +122,4 @@ def remove_status_effects(guild_id, user_id):
         if guild_id in status_dict and user_id in status_dict[guild_id]:
             del status_dict[guild_id][user_id]
 
-def update_leaderboard(guild_id, user_id, points, kill=0, death=0):
-    leaderboard.setdefault(guild_id, {})
-    leaderboard[guild_id].setdefault(user_id, {"degats": 0, "soin": 0, "kills": 0, "morts": 0})
-    leaderboard[guild_id][user_id]["degats"] += points
-    leaderboard[guild_id][user_id]["kills"] += kill
-    leaderboard[guild_id][user_id]["morts"] += death
+
