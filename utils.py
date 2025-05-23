40b15b4ff8c6f84bb8895a02169f3039e48920f5
Restimon <nico060901@gmail.com>
2025-05-23 02:23:07 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-from data import is_on_cooldown, cooldowns, ATTACK_COOLDOWN, HEAL_COOLDOWN, remove_status_effects
+from data import is_on_cooldown, cooldowns, ATTACK_COOLDOWN, HEAL_COOLDOWN, remove_status_effects, update_leaderboard
 from storage import hp, leaderboard
 
 # Objets disponibles
