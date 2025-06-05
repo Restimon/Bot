2f6d7f90a85aa0df4f47dfca3c8708ade3379f6d
Restimon <nico060901@gmail.com>
2025-06-06 01:16:01 +0200

    Update statuts.py

--- statuts.py
+++ statuts.py
@@ -1,38 +1,44 @@
 import time
 from data import virus_status, poison_status, infection_status, regeneration_status
 
+TICK_DURATION = 1800  # 30 minutes
+
 def appliquer_poison(guild_id, user_id, channel_id, source_id=None):
+    now = time.time()
     poison_status.setdefault(guild_id, {})[user_id] = {
-        "start": time.time(),
+        "start": now,
         "duration": 3 * 3600,
-        "last_tick": 0,
+        "next_tick": now + TICK_DURATION,
         "source": source_id,
         "channel_id": channel_id
     }
 
 def appliquer_infection(guild_id, user_id, channel_id, source_id=None):
+    now = time.time()
     infection_status.setdefault(guild_id, {})[user_id] = {
-        "start": time.time(),
+        "start": now,
         "duration": 3 * 3600,
-        "last_tick": 0,
+        "next_tick": now + TICK_DURATION,
         "source": source_id,
         "channel_id": channel_id
     }
 
 def appliquer_virus(guild_id, user_id, channel_id, source_id=None):
+    now = time.time()
     virus_status.setdefault(guild_id, {})[user_id] = {
-        "start": time.time(),
+        "start": now,
         "duration": 6 * 3600,
-        "last_tick": 0,
+        "next_tick": now + TICK_DURATION,
         "source": source_id,
         "channel_id": channel_id
     }
 
 def appliquer_regen(guild_id, user_id, channel_id, source_id=None):
+    now = time.time()
     regeneration_status.setdefault(guild_id, {})[user_id] = {
-        "start": time.time(),
+        "start": now,
         "duration": 3 * 3600,
-        "last_tick": 0,
+        "next_tick": now + TICK_DURATION,
         "source": source_id,
         "channel_id": channel_id
     }
