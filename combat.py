9d21a5e7e2726a7b82e92a6a3f68f48a0a26dd3f
Restimon <nico060901@gmail.com>
2025-06-05 20:28:05 +0200

    Update combat.py

--- combat.py
+++ combat.py
@@ -4,7 +4,7 @@ import discord
 
 from data import hp, leaderboard, virus_status, poison_status, infection_status, immunite_status, shields, casque_status, sauvegarder
 from utils import get_mention, get_evade_chance
-from main import handle_death, appliquer_poison, appliquer_infection, appliquer_virus
+from statuts import appliquer_poison, appliquer_infection, appliquer_virus, appliquer_regen, supprimer_tous_statuts
 from embeds import build_embed_from_item, build_embed_transmission_virale
 from cooldowns import is_on_cooldown, set_cooldown
 
