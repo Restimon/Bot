b87c76509a7026fd22c6c7bb1ad1a50dea75cf99
Restimon <nico060901@gmail.com>
2025-06-05 12:05:21 +0200

    Update admin.py

--- admin.py
+++ admin.py
@@ -5,7 +5,7 @@ from storage import inventaire, hp, leaderboard, get_user_data
 from utils import OBJETS
 from config import get_config, save_config, get_guild_config
 from data import sauvegarder, virus_status, poison_status, leaderboard
-from special_supply import send_special_supply, last_active_channel
+from special_supply import send_special_supply
 from embeds import build_embed_from_item
 from leaderboard_utils import update_leaderboard
 
