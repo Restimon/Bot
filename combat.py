db2e120c3a9801943458fccdd3fe5f8fb6761706
Restimon <nico060901@gmail.com>
2025-06-07 01:04:10 +0200

    Update combat.py

--- combat.py
+++ combat.py
@@ -7,7 +7,6 @@ from utils import get_mention, get_evade_chance
 from statuts import appliquer_poison, appliquer_infection, appliquer_virus, appliquer_regen, supprimer_tous_statuts
 from embeds import build_embed_from_item, build_embed_transmission_virale
 from cooldowns import is_on_cooldown, set_cooldown
-from inventory import voler_objet
 
 ### 🔧 UTILITAIRES GÉNÉRAUX
 
