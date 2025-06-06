5fd8526a547bdad072396cc41475c610aba934f9
Restimon <nico060901@gmail.com>
2025-06-07 01:33:50 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -5,8 +5,8 @@ from discord import app_commands
 from storage import get_user_data, leaderboard
 from data import (
     virus_status, poison_status, infection_status,
-    shields, esquive_bonus, casque_status, immunite_status,
-    regeneration_status, esquive_bonus  # ✅ Ajout ici
+    shields, esquive_status, casque_status, immunite_status,
+    regeneration_status,   # ✅ Ajout ici
 )
 from embeds import build_embed_from_item
 
