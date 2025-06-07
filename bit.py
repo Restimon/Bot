4002f1e3eea97d3a7f4b23d0a5762d970cfa7397
Restimon <nico060901@gmail.com>
2025-06-07 05:08:44 +0200

    Update bit.py

--- bit.py
+++ bit.py
@@ -4,10 +4,19 @@ from discord import app_commands
 
 # Liste de GIFs pour la morsure (tu peux en rajouter)
 BITE_GIFS = [
-    "https://media.tenor.com/mrvbxXNswb4AAAAC/anime-bite.gif",
-    "https://media.tenor.com/9-ZO-1jB4uYAAAAC/anime-bite.gif",
-    "https://media.tenor.com/l_El4-dx0D8AAAAC/anime-bite.gif",
-    "https://media.tenor.com/HkWwPBWbPTwAAAAC/anime-bite-anime.gif"
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWI4emVzaGJpd3FkbnA4cmZhNDBuOXBqcHM1OWtoZGZpeXY5ZDB6cSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/lrMUMn9lnpaJDsvP0u/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWI4emVzaGJpd3FkbnA4cmZhNDBuOXBqcHM1OWtoZGZpeXY5ZDB6cSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YW3obh7zZ4Rj2/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWI4emVzaGJpd3FkbnA4cmZhNDBuOXBqcHM1OWtoZGZpeXY5ZDB6cSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/69159EHgBoG08/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmFodzNlMzlpc3ppYXp1Z25kM3QzeDBheHhjOHc0OGY5MjhnZ213eCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/U1wMHRq7bnuInYaVlB/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmFodzNlMzlpc3ppYXp1Z25kM3QzeDBheHhjOHc0OGY5MjhnZ213eCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/OqQOwXiCyJAmA/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3YTRpaWFjeng2M3FxYXVvanl4eW1rb3FmOGVsdDUzdG13cmEwM3MwMCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/7rAYDoEhokWEpk0vw2/giphy.gifé,
+    "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3ZzlhMnVvY2tyMXFmMWc2djZzNmphOGhzZDc5em9jYnNhYXV1dDFvNSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/ixoMvaJ2NhFgouq9aY/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3ZzlhMnVvY2tyMXFmMWc2djZzNmphOGhzZDc5em9jYnNhYXV1dDFvNSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/cDX2QcwxcV2evBGn6q/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3Y2I5Yjl4a2RpeDhjdmxoYmZleHd5cHc4MnVxZGZ4aWNwdWJiMXFzYiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/F0vhUJJkVFXPCl2jJu/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3Y25ycjU2b3dicWo2OW42NjN3b2JxYXFodGgzdGtlcW84eXAxaTZkciZlcD12MV9naWZzX3NlYXJjaCZjdD1n/idNoM77zIzcWP4Lbmz/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmFodzNlMzlpc3ppYXp1Z25kM3QzeDBheHhjOHc0OGY5MjhnZ213eCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/sXR1Lyui5SEmI/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmFodzNlMzlpc3ppYXp1Z25kM3QzeDBheHhjOHc0OGY5MjhnZ213eCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/LO9Y9hKLupIwko9IVd/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3YTk5cXVqbXczYzh2dzBuanFmaDM1a21xb3Y1YWN2a2VtZmxocnQzcSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/q8TEi7UTxas92/giphy.gif"
 ]
 
 def register_bite_command(bot):
