e80bccbadd866bba6a682c549db42823e53884b2
Restimon <nico060901@gmail.com>
2025-06-07 04:58:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ from slap import register_slap_command
 from ahelp import register_ahelp_command
 from sync_command import register_sync_command
 from utilitaire import register_utilitaire_command
+from lick import register_lick_command
 
 os.makedirs("/persistent", exist_ok=True)
 
@@ -115,6 +116,8 @@ def register_all_commands(bot):
     register_ahelp_command(bot)
     register_sync_command(bot)
     register_utilitaire_command(bot) 
+    register_lick_command(bot)
+    register_bite_command(bot)
 
 # ===================== Events ======================
 
