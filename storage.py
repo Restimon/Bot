d959f4e8e4cd88a94f5cb721053c6f21b24263c3
Restimon <nico060901@gmail.com>
2025-06-09 19:35:45 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,16 +1,20 @@
-inventaire = {}
-hp = {}
-leaderboard = {}
+inventaire = {}    # {guild_id: {user_id: [objets]}}
+hp = {}            # {guild_id: {user_id: hp_value}}
+leaderboard = {}   # {guild_id: {user_id: {"degats": X, "soin": X, "kills": X, "morts": X}}}
 
+# Accès direct à l'inventaire complet d'un serveur
 def get_inventory(guild_id):
     return inventaire.setdefault(str(guild_id), {})
 
+# Mise à jour manuelle des PV d'un joueur
 def set_hp(guild_id, user_id, value):
-    hp.setdefault(guild_id, {})
-    hp[guild_id][user_id] = value
-    
+    hp.setdefault(str(guild_id), {})
+    hp[str(guild_id)][user_id] = value
+
+# Accès direct au leaderboard d'un serveur (stats de combat uniquement)
 def get_leaderboard(guild_id):
     return leaderboard.setdefault(str(guild_id), {})
+
 
 def get_user_data(guild_id, user_id):
     gid = str(guild_id)
@@ -19,7 +23,7 @@ def get_user_data(guild_id, user_id):
     inv = inventaire.setdefault(gid, {}).setdefault(uid, [])
     pv = hp.setdefault(gid, {}).setdefault(uid, 100)
 
-    # Initialisation complète des stats avec degats, soin, kills, morts
+    # Initialisation complète des stats de COMBAT
     stats = leaderboard.setdefault(gid, {}).setdefault(uid, {
         "degats": 0,
         "soin": 0,
@@ -27,7 +31,7 @@ def get_user_data(guild_id, user_id):
         "morts": 0
     })
 
-    # Correction éventuelle si stats existantes incomplètes
+    # Correction éventuelle (si stats incomplètes)
     if not isinstance(stats, dict):
         stats = {"degats": 0, "soin": 0, "kills": 0, "morts": 0}
         leaderboard[gid][uid] = stats
