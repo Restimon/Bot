6aa023cabffe5893582e19c96e5a5328312e5e37
Restimon <nico060901@gmail.com>
2025-09-11 10:55:29 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -70,6 +70,31 @@ OBJETS = {
     "⭐️": {"type": "immunite", "duree": 2 * 3600, "rarete": 22},
 }
 
+GIFS = {
+    "❄️": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3NlcTYyZDVkMjhpY3dpbmVhaXB2OXRoZGNxMHp2d3dnMmhldWR4OSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sTUe8s1481gY0/giphy.gif",
+    "🪓": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeTNtZnJ4MDd2eGpodmZnYzhpcXJlZjJsbmUwMmY2cjg2cmF5YjgxeSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/oFVr84BOpjyiA/giphy.gif",
+    "🔥": "https://i.gifer.com/MV3z.gif",
+    "⚡": "https://act-webstatic.hoyoverse.com/upload/contentweb/2023/02/02/9ed221220865a923de00661f5f9e7dea_7010733949792563480.gif",
+    "🔫": "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fa9bj0bk6ssu61.gif",
+    "🧨": "https://tenor.com/fr/view/konosuba-explosion-anime-gif-23322221",
+    "☠️": "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3Nqc3E1dWpwaHFxcjFsMGdodm5ndmIxMjh1ZW10bXc4dDV3bXB4dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qtkfPuvX4wrSuW5Q4T/giphy.gif",
+    "🦠": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWwyNjRldHBvNWNtaDk2eTMybjkxOHIwajB6b3Jxd3Q1cWVjcWNwZyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/7htcRg2IORvgKBKryu/giphy.gif",
+    "🧪": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXpubGQwd3A5am11eHN4YmQwaHh3aWtxNnpxN296NGl6aG5xbnB2YSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/9lHsP26ijVJwylXsff/giphy.gif",
+    "🧟": "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWdlZzNoYmdjd29lY29qbjE1OWY0M2gwOXRuZXlqdXVnYXh0ZWttaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UB1ZaCan78Ydj67IQl/giphy.gif",
+    "🍀": "https://64.media.tumblr.com/858f7aa3c8cee743d61a5ff4d73a378f/tumblr_n2mzr2nf7C1sji00bo1_500.gifv",
+    "🩸": "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdDhub24zeTFxbnk4ZHJ0bHkxbXVxOTVlZjljYzd0dGR5dGk4cWU1ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jN07m6w9uuZAeh80z0/giphy.gif",
+    "🩹": "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmJzam1wdTU2d3hvZjR4anhtYTdqeWFnY3M1eXdzNnRvZ2lncWViOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/EwKe1XdABwk2yvTyQt/giphy.gif",
+    "💊": "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2F9ojmyg3npkl91.gif",
+    "💕": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGw5dGZkZHp2azRjNzU5Zms1dzFkM21wczYzajA5OWk2ZjAycTNnYyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/CoKJD9a9pxc9W/giphy.gif",
+    "🔍" "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2Q0MGh0MmxzbWJkZW41d3ZremZudm8xaWsxbnBveW1vbmpvNDc3YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1fih1TYYBONo0Dkdmx/giphy.gif",
+    "💉": "https://static.wikia.nocookie.net/bokunoheroacademia/images/1/11/Heal.gif/revision/latest?cb=20180910131529",
+    "🛡": "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTkzb3hiNnR5Y2F1dmxycTl1OXZ6OWY0dTJ0eXoxdDlnMjZteDJmaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/rR7wrU76zfWnf7xBDR/giphy.gif",
+    "👟": "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzEwNngwZzk4ejd2ejg1YTZ4YWtkYmQzaG4zdW9lbzFvZThsNnp6MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/EBiho5DrxUQ75JMcq7/giphy.gif",
+    "🪖": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExN2I1amJ0eDgyZGVjZWYwdG5yYmF5ZnVjZTR2ZTNwZDlha2QwZGFnciZlcD12MV9naWZzX3NlYXJjaCZjdD1n/a0BE1Myry8SsR61BP7/giphy.gif",
+    "⭐️": "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeXhkencxNTVud25lbzd6OHkyNHd3MXdtMWx4dm5tNm04MmNqanhpeSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/2GBfKwJ7bypANDoqRt/giphy.gif",
+    "soin_autre": "https://i.makeagif.com/media/9-05-2023/UUHN2G.gif"
+}
+
 REWARD_EMOJIS = ["💰"]
 
 # =========================
@@ -205,4 +230,5 @@ def update_leaderboard(guild_id: str, user_id: str, points_delta: int = 0, kill:
         entry["points"] = int(points_delta)
         entry["kills"] = int(kill)
         entry["deaths"] = int(death)
+
 
