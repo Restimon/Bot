fd6fb25c100b982860dada68e3be9179c2ad0311
Restimon <nico060901@gmail.com>
2025-10-13 14:08:32 +0200

    Delete GotValis/Restimon_botfix-main directory

--- GotValis/Restimon_botfix-main/config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const config = {
-  token: process.env.DISCORD_TOKEN,
-  clientId: process.env.CLIENT_ID,
-  mongoUri: process.env.MONGO_URI,
-  openaiApiKey: process.env.OPENAI_API_KEY,
-};
