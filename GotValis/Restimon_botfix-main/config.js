169f5bb2a381dc225f1a0c22bd9938115ca311ba
Restimon <nico060901@gmail.com>
2025-10-14 18:53:42 +0200

    Update config.js

--- GotValis/Restimon_botfix-main/config.js
+++ GotValis/Restimon_botfix-main/config.js
@@ -4,6 +4,7 @@ dotenv.config();
 export const config = {
   token: process.env.DISCORD_TOKEN,
   clientId: process.env.CLIENT_ID,
-  mongoUri: process.env.MONGO_URI,
+  guildId: process.env.GUILD_ID,                    
+  mongoUri: process.env.MONGO_URI || process.env.MONGODB_URI, 
   openaiApiKey: process.env.OPENAI_API_KEY,
 };
